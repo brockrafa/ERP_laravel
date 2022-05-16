b531148ef9f2ed55338443c48717953b935d347d
Rafael <rafaelsuporte99@hotmail.com>
2022-05-15 23:00:25 -0300

    Criação header e boy padrão

---
+++ resources/views/app/desenv.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.layout_default')
+
+<div class="container">
+    <div class="header-secao">
+        <h4>
+            <img src="{{ asset('icones/icone-checklist.svg') }}">
+            Chamados</h4>
+    </div>
+    <div class="body-secao">
+        Filtros
+    </div>
+    <div class="body-secao">
+        teste
+    </div>
+</div>
---
+++ resources/views/app/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.layout_default')
+@section('title','Bem vindo')
+@section('content')
+  <h1>Ola galera</h1>  
+@endsection
